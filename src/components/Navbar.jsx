2a8d6db6b56927a8ffd5598021cc51b307f9f28e
FelineJTD <16520335@std.stei.itb.ac.id>
2022-06-25 16:58:34 +0700

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "/src/assets/images/braga-logo.svg";
 function Navbar() {
 
   return (
-    <nav className="flex bg-white bg-opacity-80 backdrop-blur-md rounded-md border-black py-4 px-6 border-2 fixed top-6 w-[80%] mx-[5%] 2xl:mx-[10%] justify-between z-50">
+    <nav className="flex bg-white bg-opacity-80 backdrop-blur-md rounded-md border-black py-4 px-6 border-2 fixed top-6 w-[90%] 2xl:w-[80%] mx-[5%] 2xl:mx-[10%] justify-between z-50">
       <div className="flex items-center">
         <img src={logo} alt="" className="w-4 h-4 self-start" />
         <p>GeoDashboard</p>
