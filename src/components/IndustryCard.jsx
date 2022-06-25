af5168978720e6ce89f9082bd2decc982ca02aa2
FelineJTD <16520335@std.stei.itb.ac.id>
2022-06-25 15:25:34 +0700

    try icons

--- src/components/IndustryCard.jsx
+++ src/components/IndustryCard.jsx
@@ -1,5 +1,5 @@
 const IndustryCard = (props) => {
-  const pathToIcons = "../src/assets/images/icons/"
+  const pathToIcons = "/src/assets/images/icons/"
   return (
     <div className="border-2 border-black border-opacity-25 rounded-md p-3 mx-3 text-center">
       <lord-icon trigger="hover" src={pathToIcons + props.icon} />
