0314bd6407d220ec4fba6a807a8e0386b044a1a9
FelineJTD <16520335@std.stei.itb.ac.id>
2022-07-11 17:19:23 +0700

    add robots.txt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className="flex bg-white bg-opacity-80 backdrop-blur-md rounded-md border-black py-4 px-4 md:px-6 border-2 fixed top-6 w-[90%] 2xl:w-[80%] mx-[5%] 2xl:mx-[10%] justify-between z-50">
       <div className="md:flex items-center hidden">
         <img src={logo} alt="" className="w-4 h-4 self-start" />
-        <p>GeoDashboard</p>
+        <p>GeoDashboard aaa</p>
         <p className="mx-6">|</p>
         <a href="#" className="mr-6 un">Features</a>
         <a href="#" className="un">Use Case</a>
